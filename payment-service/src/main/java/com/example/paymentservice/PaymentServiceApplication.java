eb9af58f755c65dab6ff37468d5ba90bfa84ece9
sonpt <phamson191203@gmail.com>
2025-03-17 09:56:01 +0700

    demo

--- payment-service/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -10,8 +10,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication(scanBasePackages = {"com.example"})
 public class PaymentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+
 
 }
